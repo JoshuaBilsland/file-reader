458099dc71ebcecdb1d29cbda31a102c7e0beb7c
Josh Bilsland <joshua.bilsland12@gmail.com>
2026-02-12 20:17:20 +0000

    feat: add reading file as list of lines

--- src/TextFileReader.java
+++ src/TextFileReader.java
@@ -1,9 +1,15 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class TextFileReader extends BaseFileReader {
     public String readAllText(String filePath) throws Exception {
         validatePath(filePath);
         return Files.readString(Path.of(filePath));
+    }
+
+    public List<String> readLines(String filePath) throws Exception {
+        validatePath(filePath);
+        return Files.readAllLines(Path.of(filePath));
     }
 }
